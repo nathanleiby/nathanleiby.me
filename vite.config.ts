4ceeaff68efc924a4c77da944e4986ad3f7ada0f
Nathan Leiby <nathanleiby@gmail.com>
2023-11-26 18:10:34 -0800

    need the CNAME in the dist/ folder
    https://github.com/tschaub/gh-pages/issues/127#issuecomment-470531237

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
-  base: "/", // (optional, just making it explicit for GH Pages deploy debugging)
 });
