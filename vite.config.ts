90294e4ad237bbe9e40c2ce3d617b4ac33b29312
Nathan Leiby <nathanleiby@gmail.com>
2023-11-26 18:08:02 -0800

    wip.. deploy to gh pages on custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
-  base: "/",
+  base: "/", // (optional, just making it explicit for GH Pages deploy debugging)
 });
