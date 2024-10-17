ea6006c997fda7894b61ca89a49425b2a96015dd
Nathan Leiby <nathanleiby@gmail.com>
2024-10-17 16:21:55 +0200

    add link to RESUME

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -11,6 +11,16 @@ export function About() {
         The best way to reach me is email:{" "}
         <a href="mailto:nathanleiby@gmail.com">nathanleiby@gmail.com</a>
       </Text>
+      <Text mt="xl" ta="center">
+        My resume can be found{" "}
+        <a
+          href="https://gist.github.com/nathanleiby/8bf2cea81d46a81fa5db7e01cf009943"
+          target="_"
+        >
+          here
+        </a>
+        .
+      </Text>
     </Container>
   );
 }
